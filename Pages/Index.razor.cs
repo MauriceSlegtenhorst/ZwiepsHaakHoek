90401677ed0816fab7e7b2cb9a100ff3cd6c7332
Maurice Slegtenhorst <maurice.slegtenhorst@outlook.com>
2022-02-19 22:21:45 +0100

    init

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,22 @@
+﻿using Contentful.Core;
+using Contentful.Core.Models;
+using Microsoft.AspNetCore.Components;
+using ZwiepsHaakHoek.Models;
+
+namespace ZwiepsHaakHoek.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        public IContentfulClient ContentFulClient { get; set; }
+
+        public ContentfulCollection<Product> Products { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            Products = await this.ContentFulClient.GetEntriesByType<Product>("product");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ZwiepsHaakHoek;
+using ZwiepsHaakHoek.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddServices(builder.Configuration);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
