76f10654d3362b893cc361b4fca8aeb3c888cbfe
Maurice Slegtenhorst <maurice.slegtenhorst@outlook.com>
2022-04-02 19:55:21 +0200

    Added initial polaroid component - unfinished

---
+++ Shared/Components/Polaroid.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using ZwiepsHaakHoek.Models;
+
+namespace ZwiepsHaakHoek.Shared.Components
+{
+    public partial class Polaroid
+    {
+        private Image _image;
+        [Parameter, EditorRequired]
+        public Func<Image> Image { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            _image = Image.Invoke();
+
+            return base.OnInitializedAsync();
+        }
+    }
+}
